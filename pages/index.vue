20316e5cebed42425f9a793a9c7a432edb16deca
mlpeck <mlpeck@arkbluecross.com>
2021-12-08 21:00:58 -0600

    building out the movies list template

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>Movies</h1>
+  <div class="wrapper">
+    <div class="header">
+      <v-icon color="#fffcff">mdi-filmstrip</v-icon>
+      <h1>Movies</h1>
+      <div class="search-form">
+        <label for="search" class="sr-only">Search</label>
+        <input type="text" name="search" id="search">
+        <button class="btn-search">
+          <v-icon xlarge color="#fffcff">mdi-magnify</v-icon>
+        </button>
+      </div>
+    </div>
+    <div class="movie-wrapper" v-if="movies.length > 0">
+      <div class="movie" v-for="movie in movies" :style="{ backgroundImage: 'url(' + movie.poster + ')' }" :key="movie.id" :id="movie.id">
+        <div class="content">
+          <h2>{{movie.title}}</h2>
+          <p>{{movie.overview}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
