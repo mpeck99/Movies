13480c3dc6382c8a163c54cbd0dbed35b40580a6
mlpeck <mlpeck@arkbluecross.com>
2021-12-09 23:15:38 -0600

    updating the title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Peck Movies',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -18,14 +18,14 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Material+Icons" rel="stylesheet'},
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fresca&family=Londrina+Solid:wght@100;300;400&display=swap' }
+      { rel: 'stylesheet', href: href='https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&family=Staatliches&display=swap" rel="stylesheet' }
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#FFFFFF' },
+  loading: { color: '#ff6b6b' },
 
   /*
   ** Global CSS
@@ -44,6 +44,13 @@ module.exports = {
   */
   modules: [
   ],
+ 
+  publicRuntimeConfig: {
+  },
+  privateRuntimeConfig: {
+    apiKey: process.env.API_KEY
+  },
+
 
   /*
   ** Build configuration
@@ -67,4 +74,5 @@ module.exports = {
   styleResources: {
     scss: []
   },
+
 }
